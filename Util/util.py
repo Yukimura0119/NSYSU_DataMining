e8d3654d6dd10599c9d7fa603ba7ee2eb8d8ded3
Yukimura <yukimuraz0119@gmail.com>
2022-06-04 22:38:16 +0800

    ResolveConflict

--- Util/util.py
+++ Util/util.py
@@ -23,19 +23,14 @@ def prepocess(arr: np.ndarray, stds: np.ndarray, means: np.ndarray):
     cnt = 0
     for col in arr:
         for i in range(len(col)):
-<<<<<<< HEAD:util.py
-            col[i] = (col[i]-mean) / \
-                std if std != 0 and not np.isnan(col[i]) else 0
+            col[i] = (col[i]-means[cnt]) / \
+                stds[cnt] if stds[cnt] != 0 and not np.isnan(col[i]) else 0
+        cnt += 1
     return arr.T
+
 
 def replace_train_label(train_label):
     train_label[train_label == 'KIRC'] = 0
     train_label[train_label == 'BRCA'] = 1
     train_label[train_label == 'LUAD'] = 2
     return np.squeeze(train_label.astype(np.int32))
-=======
-            col[i] = (col[i]-means[cnt]) / \
-                stds[cnt] if stds[cnt] != 0 and not np.isnan(col[i]) else 0
-        cnt += 1
-    return arr.T
->>>>>>> main:Util/util.py
--- cluster.py
+++ cluster.py
@@ -2,24 +2,17 @@ import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from Util.util import *
 from collections import Counter
 from sklearn.cluster import DBSCAN
 from sklearn.metrics.cluster import completeness_score, homogeneity_score, v_measure_score
 from pandas.plotting import scatter_matrix
+from Util.util import *
 
 absFilePath = os.path.abspath(__file__)
-<<<<<<< HEAD
-os.chdir( os.path.dirname(absFilePath))
-PICK = 32
-PATH1 = './Gene_Expression_DataSet/train_data.csv'
-PATH2 = './Gene_Expression_DataSet/train_label.csv'
-=======
 os.chdir(os.path.dirname(absFilePath))
 PICK = 8
 PATH1 = './Arrhythmia_DataSet/train_data.csv'
 PATH2 = './Arrhythmia_DataSet/train_label.csv'
->>>>>>> main
 RESULT = './result/'
 
 trn_data, _ = splitResult(PATH1)
@@ -42,4 +35,4 @@ uniques, counts = np.unique(trn_labl, return_counts=True)
 print(dict(zip(uniques, counts)))
 print(Counter(labels))
 #scatter_matrix(pd.DataFrame(trn_data, columns=[f'gene_{i}' for i in idx]))
-#plt.show()
+# plt.show()
