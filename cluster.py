4075c730025148b5836b25aa39a40c2c70144ac6
Jason_Not_Json <48216420+jasonkao402@users.noreply.github.com>
2022-06-05 00:40:31 +0800

    why different???

--- cluster.py
+++ cluster.py
@@ -10,21 +10,25 @@ from Util.util import *
 
 absFilePath = os.path.abspath(__file__)
 os.chdir(os.path.dirname(absFilePath))
-PICK = 32
+PICK = 8
 PATH1 = './Gene_Expression_DataSet/train_data.csv'
 PATH2 = './Gene_Expression_DataSet/train_label.csv'
 RESULT = './result/'
 
+std_mean = pd.read_csv('./std_mean.csv')
 trn_data, _ = splitResult(PATH1)
 trn_labl, _ = splitResult(PATH2, dtype=str)
 trn_labl = replace_data_label(trn_labl)
 
-s = np.std(trn_data, axis=0)
-idx = np.argpartition(s, -PICK)[-PICK:]
+stds = np.array(std_mean['std'])
+means = np.array(std_mean['mean'])
+
+idx = np.argpartition(stds, -PICK)[-PICK:]
 trn_data = trn_data[:, idx]
-trn_data = prepocess(trn_data)
+trn_data = prepocess(trn_data, stds, means)
 print(trn_data.shape)
-clustering = DBSCAN(eps=3.333, min_samples=8).fit(trn_data)
+
+clustering = DBSCAN(eps=53, min_samples=16).fit(trn_data)
 labels = clustering.labels_
 
 print("Homogeneity: %0.3f" % homogeneity_score(trn_labl, labels))
@@ -34,5 +38,7 @@ print("V-measure: %0.3f" % v_measure_score(trn_labl, labels))
 uniques, counts = np.unique(trn_labl, return_counts=True)
 print(dict(zip(uniques, counts)))
 print(Counter(labels))
-#scatter_matrix(pd.DataFrame(trn_data, columns=[f'gene_{i}' for i in idx]))
-# plt.show()
+
+scatter_matrix(pd.DataFrame(trn_data, columns=[f'gene_{i}' for i in idx]))
+plt.show()
+
