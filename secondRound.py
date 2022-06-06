a985774ba46d6109345c79e62286bcb8b4460068
Jason_Not_Json <48216420+jasonkao402@users.noreply.github.com>
2022-06-06 16:36:30 +0800

    acc 96

--- secondRound.py
+++ secondRound.py
@@ -6,7 +6,7 @@ from Util.util import *
 from collections import Counter
 
 STANDARD = 0.45
-DIM, RADIUS, MINP = 20, 145, 10
+DIM, RADIUS, MINP = 20, 80, 10
 
 absFilePath = os.path.abspath(__file__)
 os.chdir(os.path.dirname(absFilePath))
@@ -18,11 +18,11 @@ std_mean = pd.read_csv('./std_mean.csv')
 stds = np.array(std_mean['std'])
 means = np.array(std_mean['mean'])
 
-tst_data  = splitResult2('./Gene_Expression_DataSet/reduced_test_data.csv')
+tst_data  = splitResult2('./Gene_Expression_DataSet/reduced_test_data_2_32.csv')
 # inputData = splitResult2('./Gene_Expression_DataSet/processed.csv')
 tst_labl, _ = splitResult('./Gene_Expression_DataSet/test_label.csv', dtype=str)
 tst_labl = replace_data_label(tst_labl)
-true_labels = np.squeeze(tst_labl[uncertain])
+true_labels = np.squeeze(tst_labl)
 # feature sel by largest std
 # idx = np.argpartition(stds, -DIM)[-DIM:]
 # tst_data = preprocess(tst_data, stds, means)
@@ -32,7 +32,7 @@ inputData = tst_data[uncertain]
 print(tst_labl.shape, dnnPredict.shape)
 print(tst_data.shape, inputData.shape)
 
-pred_labels = myDBSCAN(inputData, RADIUS, MINP)
+pred_labels = myDBSCAN(tst_data, RADIUS, MINP)
 
 print("Homogeneity: %0.3f" % homogeneity_score(true_labels, pred_labels))
 print("Completeness: %0.3f" % completeness_score(true_labels, pred_labels))
@@ -40,7 +40,6 @@ print("V-measure: %0.3f" % v_measure_score(true_labels, pred_labels))
 
 print("original acc: ", np.count_nonzero(tst_labl == dnnPredict)/tst_labl.shape[0])
 
-print(np.dstack((true_labels, pred_labels)))
 cc = dict(Counter(pred_labels))
 print('Uncertain\nDBSCAN label: ', cc)
 uniques, counts = np.unique(true_labels, return_counts=True)
@@ -57,7 +56,8 @@ for i in cc.keys():
     print(*fit, sep='\n')
     rst = max(fit, key=lambda x: x[1])
     print(rst[0])
-    dnnPredict[uncertain[idx]] = rst[0]
+    pred_labels[idx] = dnnPredict[idx] = rst[0]
+#print(np.dstack((pred_labels, true_labels)))
 
 print("final acc: ", np.count_nonzero(tst_labl == dnnPredict)/tst_labl.shape[0])
 # print(np.dstack((tst_labl, dnnPredict)))
