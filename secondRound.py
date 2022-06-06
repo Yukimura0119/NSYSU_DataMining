adfd0b3babfc493796c0c13d5a28f4badb7c98c2
Jason_Not_Json <48216420+jasonkao402@users.noreply.github.com>
2022-06-06 15:46:42 +0800

    pca maybe bugs?

--- secondRound.py
+++ secondRound.py
@@ -6,20 +6,20 @@ from Util.util import *
 from collections import Counter
 
 STANDARD = 0.45
-DIM, RADIUS, MINP = 20, 2.125, 10
+DIM, RADIUS, MINP = 20, 145, 10
 
 absFilePath = os.path.abspath(__file__)
 os.chdir(os.path.dirname(absFilePath))
 
-std_mean = pd.read_csv('./std_mean.csv')
 dnnPredict = readListCSV('./dnnPredict.csv')
 uncertain  = readListCSV('./uncertain.csv')
 
+std_mean = pd.read_csv('./std_mean.csv')
 stds = np.array(std_mean['std'])
 means = np.array(std_mean['mean'])
 
-# tst_data, _ = splitResult('./Gene_Expression_DataSet/test_data.csv')
-inputData =  splitResult2('./Gene_Expression_DataSet/processed.csv')
+tst_data  = splitResult2('./Gene_Expression_DataSet/reduced_test_data.csv')
+# inputData = splitResult2('./Gene_Expression_DataSet/processed.csv')
 tst_labl, _ = splitResult('./Gene_Expression_DataSet/test_label.csv', dtype=str)
 tst_labl = replace_data_label(tst_labl)
 true_labels = np.squeeze(tst_labl[uncertain])
@@ -27,17 +27,20 @@ true_labels = np.squeeze(tst_labl[uncertain])
 # idx = np.argpartition(stds, -DIM)[-DIM:]
 # tst_data = preprocess(tst_data, stds, means)
 # inputData = tst_data[np.ix_(uncertain, idx)]
+inputData = tst_data[uncertain]
 
 print(tst_labl.shape, dnnPredict.shape)
-print(inputData.shape)
+print(tst_data.shape, inputData.shape)
 
 pred_labels = myDBSCAN(inputData, RADIUS, MINP)
 
 print("Homogeneity: %0.3f" % homogeneity_score(true_labels, pred_labels))
 print("Completeness: %0.3f" % completeness_score(true_labels, pred_labels))
 print("V-measure: %0.3f" % v_measure_score(true_labels, pred_labels))
+
 print("original acc: ", np.count_nonzero(tst_labl == dnnPredict)/tst_labl.shape[0])
 
+print(np.dstack((true_labels, pred_labels)))
 cc = dict(Counter(pred_labels))
 print('Uncertain\nDBSCAN label: ', cc)
 uniques, counts = np.unique(true_labels, return_counts=True)
@@ -50,10 +53,11 @@ for i in cc.keys():
         continue
     idx = np.argwhere(pred_labels == i).squeeze()
     cur = set(idx)
-    fit = [(i, 2*len(cur & s)/(len(cur)+len(s))) for i, s in sl]
+    fit = [(i, len(cur & s)/len(cur | s)) for i, s in sl]
+    print(*fit, sep='\n')
     rst = max(fit, key=lambda x: x[1])
-    print(rst)
+    print(rst[0])
     dnnPredict[uncertain[idx]] = rst[0]
 
 print("final acc: ", np.count_nonzero(tst_labl == dnnPredict)/tst_labl.shape[0])
-print(np.dstack((tst_labl, dnnPredict)))
+# print(np.dstack((tst_labl, dnnPredict)))
