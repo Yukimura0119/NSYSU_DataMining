938c2424e7d43ba3f38cbd945f9d72c60026c6f9
Yukimura <yukimuraz0119@gmail.com>
2022-06-07 21:48:35 +0800

    PCA_MODEL

--- DNN_test.py
+++ DNN_test.py
@@ -1,37 +1,30 @@
 import torch
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import os
-from Network import Net
-from Util.util import prepocess
 
-EPS = 1E-16
-STANDARD = 0.45
+from Network import Net
+
+EPS = 1E-15
+STANDARD = 0.1
+MODEL_NAME = 'pca_model_0.15.pth'
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 absFilePath = os.path.abspath(__file__)
 os.chdir(os.path.dirname(absFilePath))
-# train_data = pd.read_csv(
-#     '/home/yukimura/Workplace/NSYSU/Datamining/Final/Arrhythmia_DataSet/train_data.csv', header=None)
-# test_data = pd.read_csv(
-#     '/home/yukimura/Workplace/NSYSU/Datamining/Final/Arrhythmia_DataSet/test_data.csv', header=None)
-# test_label = pd.read_csv(
-#     '/home/yukimura/Workplace/NSYSU/Datamining/Final/Arrhythmia_DataSet/test_label.csv', header=None)
-std_mean = pd.read_csv(
-    './std_mean.csv')
+
 test_data = pd.read_csv(
-    './Gene_Expression_DataSet/test_data.csv')
+    './Gene_Expression_DataSet/reduced_test_data.csv')
 test_label = pd.read_csv(
     './Gene_Expression_DataSet/test_label.csv')
 
 net = Net().to(device)
 net.load_state_dict(torch.load(
-    './models/new_model_weights.pth'))
+    f'./models/'+MODEL_NAME))
 net.eval()
 
-test_data = test_data.drop(columns=['id'])
 test_label = test_label.drop(columns=['id'])
 test_data = np.array(test_data)
 test_label = np.array(test_label)
@@ -43,34 +36,32 @@ test_label[test_label == 'PRAD'] = 3
 test_label[test_label == 'COAD'] = 4
 test_label = test_label.astype(int)
 
-stds = np.array(std_mean['std'])
-means = np.array(std_mean['mean'])
-
 
 def entropy(data):
     entro = 0
     for i in data:
-        entro += -(i+EPS)*np.log(i+EPS)
+        entro += -i*np.log(i+EPS)
     return entro
 
 
 true_predict = 0
 all_predict = 0
 
+
 correct = 0
 total = test_data.shape[0]
-test_data = prepocess(test_data, stds, means)
+running_loss = 0.0
 for i in range(len(test_data)):
     result = net(torch.tensor(
         test_data[i], device=device, dtype=torch.float32))
     _, idx = torch.max(result.data, 0)
-    tmp = result.to(device='cpu').detach().numpy()
-    # prob = func.softmax(result, dim=-1)
-    # prob = prob.to(device='cpu').detach().numpy()
 
-    tmp = tmp-np.min(tmp)
-    prob = tmp/np.sum(tmp)
+    tmp = result.to(device='cpu').detach().numpy()
+    a = np.min(tmp)
+    b = np.max(tmp)
+    prob = (tmp-a)/(b-a)
     entro = entropy(prob)
+
     std = np.std(tmp)
     if entro > STANDARD:
         all_predict += 1
@@ -84,7 +75,6 @@ for i in range(len(test_data)):
         correct += 1
     else:
         plt.scatter(test_label[i], entro, c="red")
-
 print(f'Correct: {correct}')
 print(
     f'Accuracy(no unknown class): {100 * correct // total} %')
--- Network.py
+++ Network.py
@@ -6,18 +6,18 @@ import torch.nn.functional as func
 class Net(nn.Module):
     def __init__(self) -> None:
         super().__init__()
-        self.l1 = nn.Linear(20531, 4096)
-        self.l2 = nn.Linear(4096, 1024)
-        self.l3 = nn.Linear(1024, 256)
-        self.l4 = nn.Linear(256, 32)
-        self.l5 = nn.Linear(32, 3)
+        self.fc1 = nn.Linear(256, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, 32)
+        self.fc4 = nn.Linear(32, 16)
+        self.fc5 = nn.Linear(16, 3)
 
     def forward(self, x: torch.Tensor):
-        x = func.relu(self.l1(x))
-        x = func.relu(self.l2(x))
-        x = func.relu(self.l3(x))
-        x = func.relu(self.l4(x))
+        x = func.relu(self.fc1(x))
+        x = func.relu(self.fc2(x))
+        x = func.relu(self.fc3(x))
+        x = func.relu(self.fc4(x))
 
         # with softmax
         # return func.softmax(self.l3(x), dim=-1)
-        return self.l5(x)
+        return self.fc5(x)
